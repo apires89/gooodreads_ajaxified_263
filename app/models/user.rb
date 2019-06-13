63d1cc2c33bb32acf2a5fc8a14e4a09c561ca664
Andre Pires <andrecabralpires@gmail.com>
2019-06-13 21:52:47 +0200

    goodreads

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :reviews
+  #has_many :favorites
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def reviewed_book?(book)
+    reviews.where(book: book).any?
+  end
+
+  # def favorited_book?(book)
+  #   favorites.where(book: book).any?
+  # end
+
+  # def find_favorite(book)
+  #   favorites.where(book: book).first
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'books#index'
+
+  resources :books, only: :show do
+    resources :reviews, only: :create
+    resources :favorites, only: :create
+  end
+
+  resources :favorites, only: :destroy
 end
